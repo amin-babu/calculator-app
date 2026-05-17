23904e7e141988710e7215ad224abe7f44d22ff9
aminbabu12 <badrulaminbabu@gmail.com>
2026-05-18 00:17:28 +0600

    functionality done

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
-function deleteMe() { }
+function deleteMe() {
+  document.getElementById('input').value = '';
+}
 
-function calculator() { }
+function calculator(newValue) {
+  document.getElementById('input').value += newValue;
+}
 
-function answer() { }
+function cut() {
+  let a = document.getElementById('input').value;
+  let cutA = a.slice(0, -1);
+  document.getElementById('input').value = cutA;
+}
+
+function answer() {
+  let a = document.getElementById('input').value;
+  let b = eval(a);
+  document.getElementById('input').value = b;
+}
